dd1b6499898e89263d9c3795d0f39147a1d0b931
EleazarF <eleazar.fuentes@gmail.com>
2023-11-12 21:54:27 -0600

    Update App.jsx
    Create an Apollo Provider to make every request work with the Apollo server.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,30 @@
-import './App.css';
+// Import necessary dependencies from @apollo/client
+import { ApolloClient, InMemoryCache, ApolloProvider, HttpLink } from '@apollo/client';
+import React from 'react';
 import { Outlet } from 'react-router-dom';
-
+import './App.css';
 import Navbar from './components/Navbar';
 
+// Create an httpLink that connects to your server URL
+const httpLink = new HttpLink({
+  uri: 'your-graphql-server-uri', // Replace 'your-graphql-server-uri' with your actual URI
+});
+
+// Initialize Apollo Client with the HttpLink and a new instance of InMemoryCache
+const client = new ApolloClient({
+  link: httpLink,
+  cache: new InMemoryCache(),
+});
+
 function App() {
+  // Wrap the components with ApolloProvider and pass the client as a prop
   return (
-    <>
-      <Navbar />
-      <Outlet />
-    </>
+    <ApolloProvider client={client}>
+      <>
+        <Navbar />
+        <Outlet />
+      </>
+    </ApolloProvider>
   );
 }
 
